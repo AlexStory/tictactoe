85e9a03d6e47fdb5b7a4af2ad37441dc88d9650d
Alex Story <astory11@yahoo.com>
2014-12-03 15:26:04 -0600

    updated features

--- js/main.js
+++ js/main.js
@@ -1,178 +1,125 @@
 ;var tac = (function(){
   'use strict';
-
   var ref = new Firebase("https://tictactoe-alex.firebaseio.com");
   var playerTurn;
   var who;
   var board;
   var game = ref.child('game');
-  var $begin = $('.begin');
-  var $infoBox = $('.info_box');
   var myGame;
   var myPlayer;
+  var gameOver;
+  var $begin = $('.begin');
+  var $infoBox = $('.info_box p');
+    function _boardChange(){
+      board.on('child_added', function(childSnapshot, prevChildName) {
+        var td = '.' + childSnapshot.key();
+        $(td).addClass(childSnapshot.val());
+      })
+    };
 
-  $begin.click(function(){
-    game.once('value', function(dataSnapshot){
-      var found = false
-      dataSnapshot.forEach(function(childSnapshot){
-        if(childSnapshot.val() === false){
-          myGame = childSnapshot.ref();
-          myGame.set(true);
-          board = myGame.child('board');
-          playerTurn = myGame.child('player');
-          found = true;
-          myPlayer = 'x';
-          myGame.child('player').set('o');
+    function _playerChange(){
+      playerTurn.on("value", function(snapshot) {
+         who = snapshot.val();
+         if(myPlayer === who){
+           $infoBox.text('It is your turn!');
+         } else {
+           $infoBox.text('Waiting for opponent...')
+         }
+         tac.checkGameOver();
+      });
+    };
 
-          board.on('child_added', function(childSnapshot, prevChildName) {
-            var td = '.' + childSnapshot.key();
-            $(td).addClass(childSnapshot.val());
-          })
+    function _setVariables(){
+      board = myGame.child('board');
+      myGame.onDisconnect().remove();
+      playerTurn = myGame.child('player');
+      _boardChange();
+      _playerChange();
+    };
 
-          playerTurn.on("value", function(snapshot) {
-             who = snapshot.val();
-             if(myPlayer === who){
-               $infoBox.text('It is your turn!');
-             } else {
-               $infoBox.text('Waiting for opponent...')
-             }
-             tac.checkDiagonal();
-             tac.checkHorizontal();
-             tac.checkVertical();
-          });
+    $begin.click(function(){
+      if(board){
+        tac.clearBoard();
+        gameOver = false;
+      }
+      game.once('value', function(dataSnapshot){
+        var found = false
+        dataSnapshot.forEach(function(childSnapshot){
+          if(childSnapshot.val() === false){
+            myGame = childSnapshot.ref();
+            myGame.set(true);
+            found = true;
+            myPlayer = 'x';
+            myGame.child('player').set('o');
+            _setVariables();
+          }
+        })
+        if(!found){
+          myGame = game.push();
+          myGame.set(false);
+          myPlayer = 'o';
+          $infoBox.text('waiting for another player');
+          _setVariables();
         }
       })
-      if(!found){
-        myGame = game.push();
-        myGame.set(false);
-        board = myGame.child('board');
-        playerTurn = myGame.child('player');
-        myPlayer = 'o';
-        $infoBox.text('waiting for another player');
-
-        board.on('child_added', function(childSnapshot, prevChildName){
-          var td = '.' + childSnapshot.key();
-          $(td).addClass(childSnapshot.val());
-        })
-
-        playerTurn.on("value", function(snapshot) {
-           who = snapshot.val();
-           if(myPlayer === who){
-             $infoBox.text('It is your turn!');
-           } else {
-             $infoBox.text('Waiting for opponent...')
-           }
-           tac.checkDiagonal();
-           tac.checkHorizontal();
-           tac.checkVertical();
-        });
-      }
+      $(this).hide();
     })
 
-    $(this).hide();
-  })
+    $('td').click(function(){
+      if(who != myPlayer || !who || $(this).hasClass('x') || $(this).hasClass('o') || gameOver){
+        return
+      }
+      var spot = $(this).data('cell')
+      board.child(spot).set(who);
+      var it = who === 'x' ? 'o' : 'x' ;
+      playerTurn.set(it);
+    })
 
-  $('td').click(function(){
-    console.log(who + ' ' + myPlayer)
-    if(who != myPlayer || !who){
-      return
+    function endGame(lett){
+      $infoBox.text(lett.toUpperCase() + "\'s Win! \n New Game?");
+      gameOver = true;
+      var $newGame = $('<button>New Game?</button>');
+      $begin.show();
     }
-    if($(this).hasClass('x') || $(this).hasClass('o')){
-      return;
-    }
-    var spot = $(this).data('cell')
-    board.child(spot).set(who);
-
-    if(who === 'x'){
-      playerTurn.set('o');
-    } else {
-      playerTurn.set('x');
-    }
-  })
 
   return {
-    checkDiagonal : function(){
-      if($('.1').hasClass('o') && $('.5').hasClass('o') && $('.9').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.3').hasClass('o') && $('.5').hasClass('o') && $('.7').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.3').hasClass('x') && $('.5').hasClass('x') && $('.7').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.1').hasClass('x') && $('.5').hasClass('x') && $('.9').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
+    checkDiagonal : function(lett){
+      if($('.1').hasClass(lett) && $('.5').hasClass(lett) && $('.9').hasClass(lett)){
+        endGame(lett);
+      } else if ($('.3').hasClass(lett) && $('.5').hasClass(lett) && $('.7').hasClass(lett)){
+        endGame(lett);
       }
     },
-
-    checkHorizontal : function(){
-      if($('.1').hasClass('o') && $('.2').hasClass('o') && $('.3').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.4').hasClass('o') && $('.5').hasClass('o') && $('.6').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.7').hasClass('o') && $('.8').hasClass('o') && $('.9').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.1').hasClass('x') && $('.2').hasClass('x') && $('.3').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.4').hasClass('x') && $('.5').hasClass('x') && $('.6').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.7').hasClass('x') && $('.8').hasClass('x') && $('.9').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
+    checkHorizontal : function(lett){
+      if($('.1').hasClass(lett) && $('.2').hasClass(lett) && $('.3').hasClass(lett)){
+        endGame(lett);
+      } else if ($('.4').hasClass(lett) && $('.5').hasClass(lett) && $('.6').hasClass(lett)){
+        endGame(lett);
+      } else if ($('.7').hasClass(lett) && $('.8').hasClass(lett) && $('.9').hasClass(lett)){
+        endGame(lett);
       }
     },
-
-    checkVertical : function(){
-      if($('.1').hasClass('o') && $('.4').hasClass('o') && $('.7').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.2').hasClass('o') && $('.5').hasClass('o') && $('.8').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.3').hasClass('o') && $('.6').hasClass('o') && $('.9').hasClass('o')){
-        $infoBox.text("Os win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.1').hasClass('x') && $('.4').hasClass('x') && $('.7').hasClass('x')){
-        $infoBox.text("Xs win! ")
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.2').hasClass('x') && $('.5').hasClass('x') && $('.8').hasClass('x')){
-        $infoBox.text("Xs win! ");
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
-      } else if ($('.3').hasClass('x') && $('.6').hasClass('x') && $('.9').hasClass('x')){
-        $infoBox.text("Xs win! ");
-        $infoBox.append('<button>New Game?</button>');
-          $('button').addClass('newGame');
+    checkVertical : function(lett){
+      if($('.1').hasClass(lett) && $('.4').hasClass(lett) && $('.7').hasClass(lett)){
+        endGame(lett);
+      } else if ($('.2').hasClass(lett) && $('.5').hasClass(lett) && $('.8').hasClass(lett)){
+        endGame(lett);
+      } else if ($('.3').hasClass(lett) && $('.6').hasClass(lett) && $('.9').hasClass(lett)){
+        endGame(lett);
       }
     },
-
     clearBoard : function(){
-      if(board){
-        board.remove();
-      }
+      myGame.remove()
       $('td').removeClass('x');
       $('td').removeClass('o');
+    },
+    checkGameOver : function(){
+      tac.checkDiagonal('o');
+      tac.checkDiagonal('x');
+      tac.checkVertical('o');
+      tac.checkVertical('x');
+      tac.checkHorizontal('o');
+      tac.checkHorizontal('x');
     }
   }
 })();
