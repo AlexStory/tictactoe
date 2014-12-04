f421f753225882e401cc5d8d7ea8a9f71cd37744
Alex Story <astory11@yahoo.com>
2014-12-04 09:44:46 -0600

    pulling modules(unfinished)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-;var tac = (function(){
+;var main = (function(){
   'use strict';
   var ref = new Firebase("https://tictactoe-alex.firebaseio.com");
   var playerTurn;
@@ -88,58 +88,13 @@
       playerTurn.set(it);
     })
 
-    function endGame(lett){
+    function _endGame(lett){
       $infoBox.text(lett.toUpperCase() + "\'s Win! \n New Game?");
       gameOver = true;
       $begin.show();
     }
-
-  return {
-    checkDiagonal : function(lett){
-      if($('.1').hasClass(lett) && $('.5').hasClass(lett) && $('.9').hasClass(lett)){
-        endGame(lett);
-      } else if ($('.3').hasClass(lett) && $('.5').hasClass(lett) && $('.7').hasClass(lett)){
-        endGame(lett);
-      }
-    },
-    checkHorizontal : function(lett){
-      if($('.1').hasClass(lett) && $('.2').hasClass(lett) && $('.3').hasClass(lett)){
-        endGame(lett);
-      } else if ($('.4').hasClass(lett) && $('.5').hasClass(lett) && $('.6').hasClass(lett)){
-        endGame(lett);
-      } else if ($('.7').hasClass(lett) && $('.8').hasClass(lett) && $('.9').hasClass(lett)){
-        endGame(lett);
-      }
-    },
-    checkVertical : function(lett){
-      if($('.1').hasClass(lett) && $('.4').hasClass(lett) && $('.7').hasClass(lett)){
-        endGame(lett);
-      } else if ($('.2').hasClass(lett) && $('.5').hasClass(lett) && $('.8').hasClass(lett)){
-        endGame(lett);
-      } else if ($('.3').hasClass(lett) && $('.6').hasClass(lett) && $('.9').hasClass(lett)){
-        endGame(lett);
-      }
-    },
-    checkTie : function(){
-        if(_count > 8){
-          $infoBox.text("Tie Game! \n New Game?");
-          gameOver = true;
-          $begin.show();
-        }
-    },
-    clearBoard : function(){
-      myGame.remove();
-      $('td').removeClass('x');
-      $('td').removeClass('o');
-    },
-    checkGameOver : function(){
-      tac.checkDiagonal('o');
-      tac.checkDiagonal('x');
-      tac.checkVertical('o');
-      tac.checkVertical('x');
-      tac.checkHorizontal('o');
-      tac.checkHorizontal('x');
-      tac.checkTie();
+    return{
+      count: _count,
+      endGame: _endGame
     }
-  }
 })();
---
+++ js/tac.js
@@ -0,0 +1,50 @@
+var tac = (function(){
+  return {
+    checkDiagonal : function(lett){
+      if($('.1').hasClass(lett) && $('.5').hasClass(lett) && $('.9').hasClass(lett)){
+        main.endGame(lett);
+      } else if ($('.3').hasClass(lett) && $('.5').hasClass(lett) && $('.7').hasClass(lett)){
+        main.endGame(lett);
+      }
+    },
+    checkHorizontal : function(lett){
+      if($('.1').hasClass(lett) && $('.2').hasClass(lett) && $('.3').hasClass(lett)){
+        main.endGame(lett);
+      } else if ($('.4').hasClass(lett) && $('.5').hasClass(lett) && $('.6').hasClass(lett)){
+        main.endGame(lett);
+      } else if ($('.7').hasClass(lett) && $('.8').hasClass(lett) && $('.9').hasClass(lett)){
+        main.endGame(lett);
+      }
+    },
+    checkVertical : function(lett){
+      if($('.1').hasClass(lett) && $('.4').hasClass(lett) && $('.7').hasClass(lett)){
+        main.endGame(lett);
+      } else if ($('.2').hasClass(lett) && $('.5').hasClass(lett) && $('.8').hasClass(lett)){
+        main.endGame(lett);
+      } else if ($('.3').hasClass(lett) && $('.6').hasClass(lett) && $('.9').hasClass(lett)){
+        main.endGame(lett);
+      }
+    },
+    checkTie : function(){
+        if(main.count > 8){
+          $infoBox.text("Tie Game! \n New Game?");
+          gameOver = true;
+          $begin.show();
+        }
+    },
+    clearBoard : function(){
+      myGame.remove();
+      $('td').removeClass('x');
+      $('td').removeClass('o');
+    },
+    checkGameOver : function(){
+      tac.checkTie();
+      tac.checkDiagonal('o');
+      tac.checkDiagonal('x');
+      tac.checkVertical('o');
+      tac.checkVertical('x');
+      tac.checkHorizontal('o');
+      tac.checkHorizontal('x');
+    }
+  }
+})();
